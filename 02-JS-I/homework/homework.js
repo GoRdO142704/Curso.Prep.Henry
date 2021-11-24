7d8c5966041f2550ba768b28e996d56266face4b
GoRdO142704 <antonio_rivero1427@hotmail.com>
2021-11-24 19:17:38 +0100

    gordo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,14 +35,14 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  console.log(x, y)
+  console.log = x, y;
   return suma;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  console.log(x, y)
+  console.log = x, y;
   return resta;
 }
 
