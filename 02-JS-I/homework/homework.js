678a738528f806ebe3f7212297f8086ecf90306e
GoRdO142704 <antonio_rivero1427@hotmail.com>
2021-11-24 23:47:16 +0100

    gordo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -59,7 +59,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  x == y;
 }
 
 function tienenMismaLongitud(str1, str2) {
