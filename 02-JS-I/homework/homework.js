b688bfa16bf006fd4fff2ba76b9a04ba81a42a1c
GoRdO142704 <antonio_rivero1427@hotmail.com>
2021-11-24 22:57:35 +0100

    test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,8 +35,7 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  console.log = x, y;
-  return suma;
+    return x, y;
 }
 
 function resta(x, y) {
