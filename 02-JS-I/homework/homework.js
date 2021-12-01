9e2fbf0a09770b76c94634acdbeb2768fc9eabc6
GoRdO142704 <antonio_rivero1427@hotmail.com>
2021-12-01 19:29:53 +0100

    test2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -175,8 +175,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-var saludo = 'hello, world!'
-console.log("hello, world!")
+  var saludo = 'hello, world!'
+  console.log("hello, world!")
 }
 
 function combinarNombres(nombre, apellido) {
